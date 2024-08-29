8cc4d93db47069153a2d1366556b9d273ffbb0a4
charles <charles.angila@student.moringaschool.com>
2024-08-29 18:42:40 +0300

    first commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+
+    num = [0]
+    if length > 1:
+        num.append(1)
+        for i in range(2, length):
+            num.append(num[i-1] + num[i-2])
+    
+    print(num)
+
+
+print_fibonacci(1)  
+
